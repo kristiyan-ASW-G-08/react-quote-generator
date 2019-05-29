66a9a2ed69bdc4575dbc9d782c0fc661f930cde5
Kristiyan Todorov <kristiyantkirchev@gmail.com>
2019-05-29 14:21:12 +0300

    Updates

---
+++ precache-manifest.e75e2dc0c98c3b410e301a2c0a598d04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9dc70e2376c34eeb109c956857a8d4a",
+    "url": "/react-quote-generator/index.html"
+  },
+  {
+    "revision": "108180e0f6309bfd9336",
+    "url": "/react-quote-generator/static/css/main.9f75d465.chunk.css"
+  },
+  {
+    "revision": "fadbb0705bdd11d4fb8f",
+    "url": "/react-quote-generator/static/js/2.5d907561.chunk.js"
+  },
+  {
+    "revision": "108180e0f6309bfd9336",
+    "url": "/react-quote-generator/static/js/main.c002d535.chunk.js"
+  },
+  {
+    "revision": "f2c802330a7e397a93fa",
+    "url": "/react-quote-generator/static/js/runtime~main.b9c71da4.js"
+  }
+]);
